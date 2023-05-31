9c257c345512834683b42d606f3cdd5b78b32662
jun10000 <39115187+jun10000@users.noreply.github.com>
2023-05-31 09:03:43 +0900

    [test] Remove old ble_keyboard_slave.pio folder

--- keyboards/test/ble_keyboard_slave.pio/include/utility.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-uint8_t get_checksum(uint8_t *bytes, int length) {
-    uint8_t sum = 0x00;
-    for (int i = 0; i < length; i++) {
-        sum += bytes[i];
-    }
-    return sum;
-}
